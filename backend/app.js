a95a938e4d23526ed642b02b40eacbbaa6aa7852
hemanthnaika <hemanthnaika22@gmail.com>
2025-09-13 11:50:43 +0530

    hero add

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://click-cart-inky.vercel.app/"],
     credentials: true,
   })
 );

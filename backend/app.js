900dd393e3cf2b218e6d00366429c4ce70cfba77
hemanthnaika <hemanthnaika22@gmail.com>
2026-01-16 17:58:48 +0530

    Url Update

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://click-cart-shop.vercel.app/"],
+    origin: ["http://localhost:5173", "https://click-cart-shop.vercel.app"],
     credentials: true,
   })
 );
